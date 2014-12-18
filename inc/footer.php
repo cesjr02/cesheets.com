bf040cd3fc57ace5fc8772e77872b1827aa7dcd2
CJ Sheets <sheets@g.harvard.edu>
2014-12-18 11:09:03 -0600

    Update includes

--- inc/footer.php
+++ inc/footer.php
@@ -1,8 +1,5 @@
-<div id="footerContent">
-	<h1 class="contact">Contact.</h1>
-	<p>Want to know more? I'd love to hear from you.</p>
-	<a href="mailto:cesjr02@gmail.com"><img src="images/contact/email.png" alt="email icon"></a>
-	<a href="https://twitter.com/cesheets"><img src="images/contact/twitter.png" alt="twitter icon"></a>
-	<a href="http://www.linkedin.com/pub/charles-sheets-jr/69/365/2b3"><img src="images/contact/linkedin.png" alt="linkedin icon"></a>
-	<p id="footerInfo">2014 <span id="footerName">CJ Sheets.</span> Web Designer/Developer</p>			
-</div><!-- end of footer content -->
+<footer class="footer navbar-fixed-bottom">
+	<div class="container">
+		<p>2014 CJ Sheets. Web Designer/Developer <span><a href="#" id="topScroll">Back to top</a></span></p>
+	</div>
+</footer>
