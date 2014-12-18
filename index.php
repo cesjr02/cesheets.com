88ee1859d2eb14344f727e9d12eabfe07238fdfe
CJ Sheets <sheets@g.harvard.edu>
2014-12-18 11:08:53 -0600

    Consolidate all pages into index

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>CJ Sheets - Portfolio</title>
-
-<!-- Bootstrap -->
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<!-- main.css -->
-<link href="css/main.css" rel="stylesheet">
-
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Charles Sheets Jr.</title>
+	<?php include("inc/head.php"); ?>
 </head>
 <body>
 	<header class="navbar navbar-inverse navbar-static-top bs-docs-nav" id="top" role="banner">
@@ -27,13 +22,10 @@
 			<nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
 				<ul class="nav navbar-nav navbar-right">
 					<li>
-					    <a href="index.php" class="active">Home</a>
+					    <a href="#" id="aboutScroll">About</a>
 					</li>
 					<li>
-					    <a href="about.php">About</a>
-					</li>
-					<li>
-					    <a href="portfolio.php">Portfolio</a>
+					    <a href="#" id="portfolioScroll">Portfolio</a>
 					</li>
 					<li>
 					    <a href="#" id="contactScroll">Contact</a>
@@ -51,7 +43,7 @@
 		</div>
 	</header>
 	
-	<div id="skills" class="container">
+	<section id="skills" class="container">
 		<div class="col-md-4">
 			<!--<img src="images/icons/web-design-icon.png" class="icon" alt="web design icon">-->
 			<h3>Web Design</h3>
@@ -73,11 +65,11 @@
 				Utilizing the HTML5-based jQuery Mobile framework to quickly create touch-friendly websites that are accessible on 					all smartphone, tablet and desktop devices.
 			</p>
 		</div>	
-	</div>
+	</section><!-- end of skills section -->
 	
-	<div id="featured">
+	<section id="portfolio">
 		<div class="container">
-			<div id="featuredText" class="col-md-6">
+			<div id="portfolioText" class="col-md-6">
 				<h2>Featured Work</h2>
 				<p>
 					Featured on the right is the mobile version to my personal website. I used a HTML5-based jQuery Mobile framework, along with custom CSS and JavaScript. The end result is a native-like mobile app, with fluid transitions and animations. A mobile redirect is controlled via htaccess file. (Desktop computers and tablets receive the full site, while smartphones are directed to the mobile version.)
@@ -89,32 +81,48 @@
 				<a href="sheetsc_pdf_portfolio.pdf" class="primary_btn">Download PDF</a>
 			</div><!-- end of featuredText div -->
 				
-			<div id="featuredImage" class="col-md-6">
+			<div id="portfolioImage" class="col-md-6">
 				<img class="img-responsive" src="images/featured-image.jpg" alt="featured work">
 			</div>
-		</div><!-- end of container -->
-	</div><!-- end of featured div -->
-
-	<footer>
-	<div id="footerContent" class="container">
+		</div><!-- end of featured container -->
+	</section><!-- end of featured section -->
+	
+	<section id="about">
+		<div class="container">
+			<div class="col-md-6">
+			<!--<img src="images/icons/education-icon.png" class="icon" alt="education icon">-->
+			<h3>Education</h3>
+			<p>
+			I earned my Associate of Applied Science degree and Web Design certificate at Western Technical College in La Crosse, WI. In 2013 I decided to further my education. I'm currently studying online to earn a Web Technologies Certificate at Harvard Extension School, Cambridge, MA.
+			</p>
+		</div>
+		<div class="col-md-6">
+			<!--<img src="images/icons/experience-icon.png" class="icon" alt="experience icon">-->
+			<h3>Experience</h3>
+			<p>
+			My background is in graphic design and web development. I enjoy using both to compliment the websites and dynamic web applications I create. As a freelancer I became proficient while working with clients and gained the knowledge to execute projects from start to finish.
+			</p>
+		</div>
+		</div><!--end of about container -->
+	</section><!-- end of about section -->	
+	
+	
+	
+	<div id="contact" class="container">
 		<h1 class="contact">Contact.</h1>
 		<p>Want to know more? I'd love to hear from you.</p>
 		<a href="mailto:cesjr02@gmail.com"><img src="images/contact/email.png" alt="email icon"></a>
 		<a href="https://twitter.com/cesheets"><img src="images/contact/twitter.png" alt="twitter icon"></a>
-		<a href="http://www.linkedin.com/pub/charles-sheets-jr/69/365/2b3"><img src="images/contact/linkedin.png" alt="linkedin icon"></a>
-		<p id="footerInfo">2014 <span id="footerName">CJ Sheets.</span> Web Designer/Developer</p>			
-	</div><!-- end of footer content -->
-	</footer>
-	                                                             
+		<a href="http://www.linkedin.com/pub/charles-sheets-jr/69/365/2b3"><img src="images/contact/linkedin.png" alt="linkedin icon"></a>			
+	</div><!-- end of contact -->	
+		
+	<?php include("inc/footer.php"); ?>                                                                           
 
-                
-
-
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="bootstrap/js/bootstrap.min.js"></script>
-<!-- Link to external JS file -->
-<script type="text/javascript" src="js/scroll.js"></script>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+	<!-- external js for scroll functionality -->
+	<script type="text/javascript" src="js/scroll.js"></script>
 </body>
 </html>
