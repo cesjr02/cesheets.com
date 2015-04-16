74d9d60f8faa65325f9ee760cd840ef01826368e
CJ Sheets <sheets@g.harvard.edu>
2015-04-15 20:10:05 -0500

    clean up scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,37 +1,33 @@
 // Allows for smooth scrolling to the contact area.
-$(window).load(function (){
+$(window).load(function () {
 	$("#contactScroll").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#contact").offset().top
 		}, 200);
 	});
-});
 
-$(window).load(function (){
+
 	$("#skillsScroll").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#skills").offset().top
 		}, 200);
 	});
-});
 
-$(window).load(function (){
+
 	$("#aboutScroll").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#about").offset().top
 		}, 200);
 	});
-});
 
-$(window).load(function (){
+
 	$("#portfolioScroll").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#portfolio").offset().top
 		}, 200);
 	});
-});
 
-$(window).load(function (){
+
 	$("#topScroll").click(function() {
 		$('html, body').animate({
 			scrollTop: $("#top").offset().top
