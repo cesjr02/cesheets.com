c500374b45636cbd9e7ff8773142168580b0ca13
cesjr02 <cesjr02@gmail.com>
2018-03-29 22:45:32 -0500

    Revised Resume
    Updates to resume.php, and resume.css. Added simplified LinkedIn link to index.php

--- resume.php
+++ resume.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
-
-<html>
-
+<html lang="en">
 <head>
-
+	<meta charset="UTF-8">
 	<title>CJ Sheets - Resume</title>
 
 	<link rel="stylesheet" href="css/resume.css" type="text/css">
 
+	<!-- Google web fonts -->
 	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
 
 	<!-- icons -->
@@ -16,164 +15,263 @@
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="apple-touch-iphone-retina.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="apple-touch-ipad-retina.png" />
 	<link rel="icon" type="image/png" href="myicon.png" />
-
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-
 </head>
 
 <body id='resume'>
 
 	<div id='return'>
 		<a href='index.php'>&larr; Return</a> |
-		<a href='javascript:void(0);' onClick='window.print()'>Print</a> |
 		<a href='resume.pdf'> Download PDF</a>
 	</div>
 
 	<h1>Charles Sheets Jr</h1>
 
-	<h2>cesjr02@gmail.com</h2>
+	<h2>
+		4631 County Rd B. Apt 1<br />
+		La Crosse, WI, 54601<br />
+		(608) 386-3006<br />
+		cesjr02@gmail.com<br />
+
+	</h2>
 
 	<div id='tagline'>
 		<p>
-			“Web Development and User Experience combine my two primary interests;
-			computer science, and human psychology. I enjoy problem solving and creating
-			solutions that fulfill a particular need.”
+			A personable, talented web developer, and user experience professional with a diverse technical skill set
 		</p>
 	</div>
 
 	<h3>
-		Professional Profile
+		Web Development &amp; User Experience
 	</h3>
 
 
 		<div class='description'>
-				<p>	
-					Driven professional with a diverse technical skillset. Able
-					to work equally well in independently or group
-					environments where organization and communication is
-					essential. Thrives under pressure while maintaining the
-					focus needed to meet project deadlines. Ability to
-					develop and maintain co-worker and client working
-					relationships. Continually learning about new
-					technologies, standards, and social media trends.
-				</p>
-
 				<p>
-					<i>Skillset:</i>
-					<br />
-					User Experience Engineering, Mock-up testing, Website Design, Front &amp; Backend Web Development, Graphic Design &amp; Typography
+					Able to apply strong analytical and problem-solving skills, creativity, and resourcefulness, in solving web design and user
+					experience challenges. Performs well under pressure with ability to maintain the necessary focus to meet project deadlines.
+					Genuinely enjoys research and continued learning, to stay current with new developments in technology, and social media
+					trends.
 				</p>
 
-				
+				<h4>General &amp; Technical skills</h4>
+
+				<div>
+					<ul>
+						<li><span>
+							<b>General skills:</b> Needs analysis, user experience, mockup testing, web design, front and back-end web development,
+							branding & marketing, graphic design, and typography
+						</span></li>
+						<li><span>
+							<b>Programming:</b> HTML, CSS, XML, PHP, and JavaScript
+						</span></li>
+						<li><span>
+							<b>Frameworks:</b> Laravel
+						</span></li>
+						<li><span>
+							<b>Software:</b> Various text editors, command line, Git version control, LAMP, FTP clients, WordPress, Photoshop,
+							Illustrator, and Acrobat
+						</span></li>
+						<li><span>
+							<b>Operating systems:</b> Microsoft Windows, and macOS
+						</span></li>
+					</ul>
+				</div>
+
+
 		</div>
-
-
-	<h3>
-		Career Development
-	</h3>
-	<!-- IRIS -->
-
-		<div class='item-heading'>
-			<div class='title'>IRIS Program; Holmen, WI
-			<span class='date'>2009&ndash;2016</span></div>
-		</div>
-
-		<div class='role'>
-			Caregiver
-		</div>
-		<div>
-			<p>In-home caregiver for an immediate family member. More information available upon request.</p>
-		</div>
-
-
-	<!-- Freelance -->
-		<div class='item-heading'>
-			<div class='title'>Web Design &amp; Development
-			<span class='date'>2008&ndash;2015</span></div>
-		</div>
-
-		<div class='role'>
-			Freelance Web Programmer &amp; Designer
-		</div>
-
-		<div class='description'>
-			<p>
-				Designed, developed, and maintained client websites. Collaborated with designers and developers on many different projects utilizing the git version control system. Established working relationships with clients and businesses. Demonstrated clear understanding of HTML, CSS, XML, JavaScript and Content Management Systems. Conceived marketing strategies and brand identity.
-			</p>
-		</div>
-
 
 	<!-- Education -->
 	<h3>
 		Education
 	</h3>
-	<!-- HES -->
+		<!-- HES -->
 		<div class='item-heading'>
-			<div class='title'>Harvard Extension School<br />Cambridge, MA
-			<span class='date'>2013&ndash;2015</span></div>
+			<div class='title'>HARVARD UNIVERSITY EXTENSION SCHOOL
+			<span class='location'>Cambridge, MA</span></div>
 		</div>
 
 		<div class='role'>
 			Web Technologies Certificate
+			<span class='date'>December 2015</span>
 		</div>
-
-		<div class='description'>
+		<div>
 			<ul>
-				<li>Professional Graduate Certificate</li>
+				<li><span>
+					Earned a 4.0 GPA
+				</span></li>
+				<li><span>
+					Created a Harvard University course catalog for final project
+				</span></li>
 			</ul>
 		</div>
 
-	<!-- WTC -->
+		<!-- WTC -->
 		<div class='item-heading'>
-			<div class='title'>Western Technical College<br />La Crosse, WI
-			<span class='date'>2007&ndash;2009</span></div>
+			<div class='title'>WESTERN TECHNICAL COLLEGE
+			<span class='location'>La Crosse, WI</span></div>
 		</div>
 
 		<div class='role'>
-			Associate of Applied Science, Graphic Design
+			Associate of Applied Science in Graphic Design
+			<span class='date'>May 2009</span>
 		</div>
-
-		<div class='description'>
+		<div>
 			<ul>
-				<li>Associate Degree in Graphic Design</li>
-				<li>Web Design Certificate</li>
+				<li><span>
+					Earned a 3.6 GPA
+				</span></li>
+				<li><span>
+					Completed necessary coursework to earn a Web Design Certificate
+				</span></li>
 			</ul>
 		</div>
 
+
+	<!-- Experience -->
 	<h3>
-	Technical Skills
+		Professional Experience
 	</h3>
+	<!-- Freelance Work -->
+	<div class='item-heading'>
+		<div class='title'>FREELANCE PROFESSIONAL
+		<span class='location'>La Crosse, WI</span></div>
+	</div>
 
-		<div class='description'>
-				<p>
-					Web Programming:<br />
-					HTML, CSS, XML, JavaScript
-				</p>
-		</div>
-
-		<div class='description'>
-				<p>
-					Database:<br />
-					MySQL
-				</p>
-		</div>
-
-		<div class='description'>
-				<p>
-					Software:<br />
-					Various text editors, command line, git version control, LAMP, FTP clients, Wordpress, Photoshop, Illustrator, Acrobat
-				</p>
-		</div>
-
-		<div class='description'>
-				<p>
-					Operating Systems:<br />
-					Windows, Mac OS X
-				</p>
-		</div>
+	<div class='role'>
+		Web Designer &amp; Developer
+		<span class='date'>January 2008 &ndash; Present</span>
+	</div>
+	<div>
+		<ul>
+			<li><span>
+				Designed, developed, and maintained client websites
+			</span></li>
+			<li><span>
+				Collaborated with designers and developers on many different projects utilizing Git version control
+			</span></li>
+			<li><span>
+				Established productive working relationships with clients
+			</span></li>
+			<li><span>
+				Consulted with clients on various applications and solutions utilizing HTML, CSS, XML, JavaScript, and
+				content management systems
+			</span></li>
+			<li><span>
+				Assisted clients with branding and marketing strategies
+			</span></li>
+		</ul>
+	</div>
 
 
-	
+<!-- Featured Projects -->
+	<h3>
+	Featured Projects
+	</h3>
+	<!-- Heavy Equipment Repair -->
+	<h4>Heavy Equipment Repair, LLC, Holmen, WI</h4>
+	<div class="project-link"><a href="http://heavyequipmentrepair.net">heavyequipmentrepair.net</a></div>
+
+	<div class="indent">
+		<p>
+			Hired to provide site maintenance, server migration tasks, and general updates. Also provided assistance by correcting
+			a technical issue caused by a compromised web server; restored code, deleted malicious files, and strengthened
+			security measures.
+		</p>
+
+		<p>
+			<b>Result:</b> Client was able to devote more time to core business needs as a result of receiving technical assistance on a
+	continued basis.
+		</p>
+	</div>
+
+
+	<br />
+
+	<!-- Yapperbox -->
+	<h4>Yapperbox</h4>
+	<p class="project-desc">College course final project, Harvard University Extension School, Cambridge, MA</p>
+	<div class="project-link"><a href="http://yapperbox.sheetsc.com">yapperbox.sheetsc.com</a></div>
+
+	<div class="indent">
+		<p>
+			Developed a social media web application. This application was developed using a custom PHP framework, allowed
+			users to perform tasks that included; registering, creating a profile, uploading a avatar, posting comments, and
+			replying to comments of other users.
+		</p>
+
+		<p>
+			<b>Result:</b> Received an A for project grade
+		</p>
+	</div>
+
+
+	<br />
+
+	<!-- Harvard University Course Catalog -->
+	<h4>Harvard University Course Catalog</h4>
+	<p class="project-desc">College course final project, Harvard University Extension School, Cambridge, MA</p>
+
+	<div class="indent">
+		<p>
+			Created a dynamic searchable Harvard Course Catalog utilizing XML course data. Collaborated with a small team of
+			students utilizing Git version control software.
+		</p>
+
+		<p>
+			<b>Result:</b> Received an A for project grade
+		</p>
+	</div>
+
+
+	<br />
+
+	<!-- JavaScript Form Handler -->
+	<h4>JavaScript Form Handler</h4>
+	<p class="project-desc">College course final project, Harvard University Extension School, Cambridge, MA</p>
+	<div class="project-link"><a href="http://form.sheetsc.com/">form.sheetsc.com</a></div>
+
+	<div class="indent">
+		<p>
+			Developed an interactive web form. Application provides users with real-time validation requirements. Features
+			include; custom form validation, Google Maps Geolocation API for city and state retrieval based on zip code input,
+			and a local storage implementation to save users' non-sensitive data.
+		</p>
+
+		<p>
+			<b>Result:</b> Received an A for project grade
+		</p>
+	</div>
+
+
+	<br />
+
+	<!-- Music Management Application - User Experience -->
+	<h4>Music Management Application - User Experience</h4>
+	<p class="project-desc">College course final project, Harvard University Extension School, Cambridge, MA</p>
+	<div class="project-link"><a href="https://youtu.be/0kWhbWPDLKA">https://youtu.be/0kWhbWPDLKA</a></div>
+
+	<div class="indent">
+		<p>
+			Proposed and implemented the user experience for a Music Management Application. Tasks performed: Gathered
+			research, submitted a written proposal, conducted interviews of real-world users, created sketches for mockup testing,
+			and prepared and presented a final prototype.
+		</p>
+
+		<p>
+			<b>Result:</b> Received an A for project grade
+		</p>
+	</div>
+
+	<!-- Additional Information -->
+	<h3>Additional Information</h3>
+	<p><b>Portfolio:</b> Creative portfolio and related information can be viewed at <a href="http://cesheets.com/portfolio.pdf">cesheets.com</a></p>
+
+	<p>
+		<b>Career note:</b> Managed freelance web design and development activity while providing in-home care for an immediate
+family member from October 2009 &ndash; February 2016.
+	</p>
+
+
 
 		&nbsp;
 
